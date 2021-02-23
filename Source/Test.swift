798c444448cf1cb6b122c6f5a312876e5423edce
Thomas Johansson <thomasjohansson@Thomass-MacBook-Pro-2.local>
2021-02-23 20:56:32 +0100

    initial commit

---
+++ Source/Test.swift
@@ -0,0 +1,14 @@
+//
+//  Test.swift
+//  npios
+//
+//  Created by Thomas Johansson on 2021-02-23.
+//
+
+import Foundation
+
+public class Testing {
+    public func testString() -> String {
+        return "Hi Thomas!"
+    }
+}
